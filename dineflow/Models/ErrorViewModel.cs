a080e7c4189e4c5e6e368ea40fce501526db2a86
lester <lester@LESTER>
2025-02-19 21:14:54 +0800

    Add project files.

---
+++ dineflow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dineflow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
