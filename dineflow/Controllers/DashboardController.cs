003c559d74c5c5c8607b5a048912909af260c646
lester <lester@LESTER>
2025-02-20 23:04:45 +0800

    Dashboard-Sidebar

--- dineflow/Controllers/DashboardController.cs
+++ dineflow/Controllers/DashboardController.cs
@@ -8,4 +8,32 @@ public class DashboardController : Controller
     {
         return View(); // No need to check authentication, the [Authorize] attribute already ensures it
     }
+    public IActionResult Inventory()
+    {
+        return View(); // No need to check authentication, the [Authorize] attribute already ensures it
+    }
+    public IActionResult Management()
+    {
+        return View(); // No need to check authentication, the [Authorize] attribute already ensures it
+    }
+    public IActionResult Pos()
+    {
+        return View(); // No need to check authentication, the [Authorize] attribute already ensures it
+    }
+    public IActionResult Product()
+    {
+        return View(); // No need to check authentication, the [Authorize] attribute already ensures it
+    }
+    public IActionResult Settings()
+    {
+        return View(); // No need to check authentication, the [Authorize] attribute already ensures it
+    }
+    public IActionResult Table()
+    {
+        return View(); // No need to check authentication, the [Authorize] attribute already ensures it
+    }
+    public IActionResult Transaction()
+    {
+        return View(); // No need to check authentication, the [Authorize] attribute already ensures it
+    }
 }
